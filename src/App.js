413415642afd09b4345808cfecb4627126b960c9
Amol kapadi <amolakapadi@gmail.com>
2024-07-27 14:30:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,64 +1,34 @@
-import React, { useState } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import Sidebar from './component/Sidebar';
+// App.js
+import React from 'react';
+import CountUp from 'react-countup';
+import projectImg from './img/project.png';
+import clientImg from './img/client.png';
+import deliverImg from './img/handshake.png';
+import awardsImg from './img/trophy.png';
 import './App.css';
 
-const localizer = momentLocalizer(moment);
-
 const App = () => {
-  const [showOffcanvas, setShowOffcanvas] = useState(false);
-  const [events, setEvents] = useState([]);
-  const [selectedEvent, setSelectedEvent] = useState(null);
-
-  const toggleOffcanvas = () => {
-    setShowOffcanvas(!showOffcanvas);
-  };
-
-  const handleAddEvent = (newEvent) => {
-    setEvents([...events, newEvent]);
-    setShowOffcanvas(false);
-  };
-
-  const handleEditEvent = (updatedEvent) => {
-    setEvents(events.map(event => (event.id === updatedEvent.id ? updatedEvent : event)));
-    setShowOffcanvas(false);
-  };
-
-  const handleDeleteEvent = (id) => {
-    setEvents(events.filter(event => event.id !== id));
-    setShowOffcanvas(false);
-  };
-
-  const handleSelectEvent = (event) => {
-    setSelectedEvent(event);
-    setShowOffcanvas(true);
-  };
+  const cardData = [
+    { id: 1, title: 'Project', start: 0, end: 100, duration: 2, img: projectImg },
+    { id: 2, title: 'Happy Client', start: 0, end: 30, duration: 2.5, img: clientImg },
+    { id: 3, title: 'Project Deliver', start: 0, end: 30, duration: 3, img: deliverImg },
+    { id: 4, title: 'Awards Received', start: 0, end: 10, duration: 3, img: awardsImg },
+  ];
 
   return (
-    <div className='calender-container'>
-      <div className='btn-container'>
-        <button className="btn-event" onClick={() => { setSelectedEvent(null); toggleOffcanvas(); }}>
-          Add Event
-        </button>
+    <div className="App">
+      <h1>Animating Number Counters React Js</h1>
+      <div className="count-up-cards">
+        {cardData.map(card => (
+          <div key={card.id} className="card">
+            <img src={card.img} alt={card.title} className="card-img" />
+            <h3>{card.title}</h3>
+            <span style={{ fontSize: '3rem' }}>
+  <CountUp start={card.start} end={card.end} duration={card.duration} />
+</span>
+          </div>
+        ))}
       </div>
-      <Sidebar
-        show={showOffcanvas}
-        onHide={() => setShowOffcanvas(false)}
-        onAddEvent={handleAddEvent}
-        onEditEvent={handleEditEvent}
-        onDeleteEvent={handleDeleteEvent}
-        selectedEvent={selectedEvent}
-      />
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: '100vh' }}
-        onSelectEvent={handleSelectEvent}
-      />
     </div>
   );
 };
